d510f5b265097039ad686c71cbf9fb50e656c4c2
Hayden Carlson <hayden.carlson@icloud.com>
2023-05-12 11:35:06 -0700

    Fix redirects

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { withClerkMiddleware, getAuth } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const publicPaths = ['/'];
+const publicPaths = ['/', '/*'];
 
 const isPublic = (path: string) => {
   return publicPaths.find(x =>
