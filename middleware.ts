a0874fb3e765ed7b4de2a57db93a34d1e9b6f888
Hayden Carlson <hayden.carlson@icloud.com>
2023-05-12 12:01:52 -0700

    Update middleware to new version

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,9 @@
-import { withClerkMiddleware, getAuth } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { authMiddleware } from "@clerk/nextjs";
 
-const publicPaths = ['/'];
+export default authMiddleware({
+  publicRoutes: ["/"],
+});
 
-const isPublic = (path: string) => {
-  return publicPaths.find(x =>
-    path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
-  )
-}
-
-export default withClerkMiddleware((request: NextRequest) => {
-  if (isPublic(request.nextUrl.pathname)) {
-    return NextResponse.next()
-  }
-  // if the user is not signed in redirect them to the sign in page.
-  const { userId } = getAuth(request)
-
-  if (!userId) {
-    // redirect the users to /pages/sign-in/[[...index]].ts
-
-    const signInUrl = new URL('/sign-in', request.url)
-    signInUrl.searchParams.set('redirect_url', request.url)
-    return NextResponse.redirect(signInUrl)
-  }
-  return NextResponse.next()
-})
-
-export const config = { matcher: '/((?!_next/image|_next/static|favicon.ico).*)', };
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/'"],
+};
