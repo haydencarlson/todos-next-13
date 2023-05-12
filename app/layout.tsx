5ef701819f47acde485ad3edb8d44ec5f3ffd9c4
Hayden Carlson <hayden.carlson@icloud.com>
2023-05-12 13:04:31 -0700

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs/app-beta';
-
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from '../components/Navbar';
 
 export default function RootLayout({
@@ -13,6 +13,7 @@ export default function RootLayout({
       <html lang='en'>
         <body>
           <Navbar />
+          <Analytics />
           {children}
         </body>
       </html>
